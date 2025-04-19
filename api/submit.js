21af802e23f4688f7a51a07918eb1c6f60cca7d0
Konstantinos Stouras <kstouras@gmail.com>
2025-04-19 20:41:23 +0100

    Update submit.js

--- api/submit.js
+++ api/submit.js
@@ -1,9 +1,11 @@
 export default async function handler(req, res) {
-  res.setHeader("Access-Control-Allow-Origin", "*");
+  // CORS headers for every request
+  res.setHeader("Access-Control-Allow-Origin", "https://www.stouras.com");
   res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
 
   if (req.method === "OPTIONS") {
+    // Preflight handled
     return res.status(200).end();
   }
 
